e9f62e7eaff1e2c19963d0525b4c0906296e0aa6
Andrew Haberman <a.haberman25@gmail.com>
2020-03-19 19:30:12 -0400

    styles and added dropdown

--- user-onboarding/src/components/Form.component.jsx
+++ user-onboarding/src/components/Form.component.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components';
 
 const UserForm = props => {
     console.log('form', props)
 
+    const Title = styled.h1`
+        font-family: 'Raleway', sans-serif;
+        font-weight: 600;
+        color: #4d4d4d;
+        font-size: 2.2em;
+    `;
+
     return (
-        // - [ ] Name
-        // - [ ] Email
-        // - [ ] Password
-        // - [ ] Terms of Service (checkbox)
-        // - [ ] A Submit button to send our form data to the server.
-        <Form>
-            <Field name='name' placeholder='Your Name' />
-            {props.touched.name && props.errors.name ? (<span className="error">{props.errors.name}</span>) : null}
-            <Field name='email' placeholder='Your Email' />
-            {props.touched.email && props.errors.email ? (<span className="error">{props.errors.email}</span>) : null}
-            <Field name='password' type='password' placeholder='Create Password' />
-            {props.touched.password && props.errors.password ? (<span className="error">{props.errors.password}</span>) : null}
-            <Field name='tos' type='checkbox' />
-            {props.touched.tos && props.errors.tos ? (<span className="error">{props.errors.tos}</span>) : null}
-            <button type='submit'>Submit</button>
-        </Form>
+        <div>
+            <Title>Create User</Title>
+            <Form className='form'>
+                <Field className='field' name='name' placeholder='Your Name' />
+                {props.touched.name && props.errors.name ? (<span className="error">{props.errors.name}</span>) : null}
+                <Field className='field' name='email' placeholder='Your Email' />
+                {props.touched.email && props.errors.email ? (<span className="error">{props.errors.email}</span>) : null}
+                <Field className='field' name='password' type='password' placeholder='Create Password' />
+                {props.touched.password && props.errors.password ? (<span className="error">{props.errors.password}</span>) : null}
+                <Field className='field' as='select' name='role'>
+                    <option value='Developer'>Developer</option>
+                    <option value='Coder'>Coder</option>
+                    <option value='Manager'>Manager</option>
+                </Field>
+                {props.touched.role && props.errors.role ? (<span className="error">{props.errors.role}</span>) : null}
+                <label htmlFor='tos'>Do you accept our Terms of Service</label>
+                <Field className='field' name='tos' type='checkbox' />
+                {props.touched.tos && props.errors.tos ? (<span className="error">{props.errors.tos}</span>) : null}
+                <button className='button' type='submit'>Submit</button>
+            </Form>
+        </div>
     )
 }
 
@@ -30,6 +43,7 @@ export default withFormik({
         return {
             name: props.name || '',
             email: props.email || '',
+            role: props.role || '',
             password: props.password || '',
             tos: props.name || false
         }
@@ -37,6 +51,7 @@ export default withFormik({
     validationSchema: Yup.object().shape({
         name: Yup.string().required('Name is required to submit this form'),
         email: Yup.string().required('Email is required to submit this form').email('Please use a valid email address'),
+        role: Yup.string().required('A Role is required to submit this form'),
         password: Yup.string().required('A Password is required to submit this form'),
         tos: Yup.boolean().oneOf([true], 'Please agree to the Terms of Service')
     }),
